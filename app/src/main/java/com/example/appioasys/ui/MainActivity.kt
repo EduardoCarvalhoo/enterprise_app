4385ba6249ccf8d45b809a0814c0504cb181b56b
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2022-09-20 02:23:51 -0300

    Definindo melhorias na implementação lógica da tela de login

--- app/src/main/java/com/example/appioasys/ui/MainActivity.kt
+++ app/src/main/java/com/example/appioasys/ui/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.appioasys.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Patterns
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import com.example.appioasys.R
 import com.example.appioasys.databinding.ActivityMainBinding
-import java.util.regex.Matcher
-import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,44 +19,69 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.custom_black_23)
 
-        binding.mainProgressBar.isVisible = false
-        binding.mainEnterButton.setOnClickListener {
-            validateEmail()
-            validatePassword()
-        }
+        validateEmail()
+        configureEmailAndPasswordChange()
     }
 
     private fun validateEmail() {
-        val emailText = binding.mainEmailFieldEditText.editText?.text.toString()
-        if (Patterns.EMAIL_ADDRESS.matcher(emailText).matches() && validatePassword()) {
-            with(binding) {
-                mainEmailErrorAlertTextView.isVisible = false
-                mainPasswordErrorAlertTextView.isVisible = false
-                mainErrorMessageTextView.isVisible = false
-                mainAccessDigitsText.isEndIconVisible = true
-                mainEnterButton.setBackgroundResource(R.color.greeny_blue)
-                mainProgressBar.isVisible = true
-            }
-        } else {
-            with(binding) {
-                mainEmailErrorAlertTextView.isVisible = true
-                mainPasswordErrorAlertTextView.isVisible = true
-                mainErrorMessageTextView.isVisible = true
-                mainAccessDigitsText.isEndIconVisible = false
-                mainEnterButton.setBackgroundResource(R.color.steel_grey)
+        with(binding) {
+            mainEnterButton.setOnClickListener {
+                val emailText = mainEmailEditText.text.toString()
+                val password = mainPasswordEditText.text.toString()
+                if (Patterns.EMAIL_ADDRESS.matcher(emailText).matches() && password.length > 3) {
+                    mainProgressBar.isVisible = true
+                    navigateToHomeScreen()
+                } else {
+                    mainEmailErrorAlertTextView.isVisible = true
+                    mainPasswordErrorAlertTextView.isVisible = true
+                    mainErrorMessageTextView.isVisible = true
+                    mainPasswordFieldLayout.isEndIconVisible = false
+                    mainEnterButton.setBackgroundResource(R.color.steel_grey)
+                }
             }
         }
     }
 
-    private fun validatePassword(): Boolean {
-        val password = binding.mainAccessDigitsText.editText?.text.toString()
-        val regex = "^(?=.*[a-z])(?=." + "*[A-Z])(?=.*\\d)" + "(?=.*[-+_!@#$%^&*.,?]).+$"
-        val patterns: Pattern = Pattern.compile(regex)
-        val matcher: Matcher = patterns.matcher(password)
-        return matcher.matches()
+        private fun configureEmailAndPasswordChange() {
+            val emailText: EditText = binding.mainEmailEditText
+            val passwordText: EditText = binding.mainPasswordEditText
+            configureTextChange(emailText)
+            configureTextChange(passwordText)
+        }
+
+        private fun configureTextChange(inputText: EditText) {
+            inputText.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    with(binding) {
+                        mainEmailErrorAlertTextView.isVisible = false
+                        mainPasswordErrorAlertTextView.isVisible = false
+                        mainErrorMessageTextView.isVisible = false
+                        mainPasswordFieldLayout.isEndIconVisible = true
+                        mainEnterButton.setBackgroundResource(R.color.greeny_blue)
+                        mainProgressBar.isVisible = false
+                    }
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                }
+            })
+        }
+
+        private fun navigateToHomeScreen() {
+            val intentHome = Intent(this, HomeActivity::class.java)
+            startActivity(intentHome)
+        }
     }
-}
 
 
 
