d23002835e565cfc146c4f86f587643573e3a7bc
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2022-12-03 17:35:54 -0300

    Definindo novos ajustes para o alert dialog

--- app/src/main/java/com/example/appioasys/utils/ActivityExtensions.kt
+++ app/src/main/java/com/example/appioasys/utils/ActivityExtensions.kt
@@ -4,13 +4,11 @@ import android.app.Activity
 import android.app.AlertDialog
 import com.example.appioasys.R
 
-fun Activity.showAlertDialog(message: Int, positiveButtonAction: (() -> Unit)? = null){
+fun Activity.showAlertDialog(message: Int, positiveButtonAction: (() -> Unit)? = null) {
     AlertDialog.Builder(this)
-        .setTitle(getString(R.string.error_dialog_title_text))
         .setMessage(message)
         .setPositiveButton(getString(R.string.error_dialog_positive_button_message_text)) { _, _ ->
             positiveButtonAction?.invoke()
         }
-        .setNegativeButton(getString(R.string.error_dialog_negative_button_message_text)) { _, _ ->}
-            .show()
+        .show()
 }
